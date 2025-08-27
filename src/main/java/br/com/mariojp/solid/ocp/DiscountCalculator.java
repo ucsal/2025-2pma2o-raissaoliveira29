feeea7509c441a9878d781b24423c5e1b958494b
Raissa Vieira <raissavieira.oliveira@ucsal.edu.br>
2025-08-27 16:28:58 -0300

    Resolução da atividade

--- src/main/java/br/com/mariojp/solid/ocp/DiscountCalculator.java
+++ src/main/java/br/com/mariojp/solid/ocp/DiscountCalculator.java
@@ -1,11 +1,22 @@
 package br.com.mariojp.solid.ocp;
 
+import java.util.Map;
+
 public class DiscountCalculator {
-    public double apply(double amount, CustomerType type){
-        switch (type){
-            case REGULAR: return amount * 0.95;
-            case PREMIUM: return amount * 0.90;
-            default: return amount;
-        }
+
+    private final Map<CustomerType, DiscountPolicy> policies;
+
+    public DiscountCalculator() {
+        this.policies = Map.of(
+            CustomerType.REGULAR, new RegularPolicy(),
+            CustomerType.PREMIUM, new PremiumPolicy(),
+            CustomerType.PARTNER, new PartnerPolicy()
+        );
+    }
+    public DiscountCalculator(Map<CustomerType, DiscountPolicy> policies) {
+        this.policies = policies;
+    }
+    public double apply(double amount, CustomerType type) {
+        return policies.getOrDefault(type, a -> a).apply(amount);
     }
 }
